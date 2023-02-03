c12e5694a433deb3191af05ab666caed6afc7eca
drumfeet <drumfeet@gmail.com>
2023-02-03 20:19:38 +0800

    wip

--- sample/pages/index.js
+++ sample/pages/index.js
@@ -2,41 +2,66 @@ import { useEffect, useState } from "react";
 import SDK from "weavedb-client";
 // const SDK = require("weavedb-node-client")
 
+let clientGetOwner = 1;
+let clientGetInfo = 1;
+let clientGetVersion = 0;
+let getDataNft = 0;
 let sdk;
-const contractTxId = "f86Qw3vp6TlgxI3mABFnWDQNqR8mPVkVTBN04hwywqc";
+const contractTxId = "SMVDZ9jRXO7Kb88D4rByayB3HbwzXYzzbaPJ9DnrB_s";
 
 export default function Home() {
   const [ok, setOK] = useState(false);
+  const [owner, setOwner] = useState();
+  const [info, setInfo] = useState();
   const [version, setVersion] = useState();
 
   useEffect(() => {
     (async () => {
       sdk = new SDK({
         contractTxId,
-        /**/
+        /*weavedb-node-client*/
         // rpc: "localhost:8080",
         // rpc: "lb.weavedb-node.xyz:443",
 
-        /**/
+        /*weavedb-client*/
         rpc: "http://localhost:8080",
         // rpc: "http://lb.weavedb-node.xyz:80",
         // rpc: "https://lb.weavedb-node.xyz:443",
       });
 
-      await sdk
-        .getInfo()
-        .then((result) => {
-          setVersion(result.version);
-          console.log(result.version);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
+      const list = await sdk.get("nft", ["tokenID", "desc"], true);
+      console.log(list);
+
+      if (clientGetOwner) {
+        setOwner(await sdk.getOwner());
+      }
+
+      if (clientGetInfo) {
+        await sdk
+          .getInfo()
+          .then((result) => {
+            setVersion(result.version);
+            console.log("getInfo()", result);
+          })
+          .catch((err) => {
+            console.log("getInfo()", err);
+          });
+      }
+
+      if (clientGetVersion) {
+        await sdk.getVersion();
+      }
+
+      if (getDataNft) {
+        const list = await sdk.get("nft", ["tokenID", "desc"], true);
+        console.log(list);
+      }
     })();
   }, []);
 
   return (
     <>
+      <div>getOwner() owner : {owner}</div>
       <div>getInfo() version : {version}</div>
     </>
   );
