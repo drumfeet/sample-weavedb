672e0f8e3c0fdff608a28f021e3671d7545e676e
drumfeet <drumfeet@gmail.com>
2023-02-09 15:42:13 +0800

    wip

--- sample/pages/index.js
+++ sample/pages/index.js
@@ -4,10 +4,10 @@ import SDK from "weavedb-client";
 
 let clientGetOwner = 1;
 let clientGetInfo = 1;
-let clientGetVersion = 0;
+let clientGetVersion = 1;
 let getDataNft = 0;
 let sdk;
-const contractTxId = "SMVDZ9jRXO7Kb88D4rByayB3HbwzXYzzbaPJ9DnrB_s";
+const contractTxId = "gdZA53CmDBnxyeK437_4DtpbzW9qvXIKJZdYTaqyxDE";
 
 export default function Home() {
   const [ok, setOK] = useState(false);
@@ -19,18 +19,8 @@ export default function Home() {
     (async () => {
       sdk = new SDK({
         contractTxId,
-        /*weavedb-node-client*/
-        // rpc: "localhost:8080",
-        // rpc: "lb.weavedb-node.xyz:443",
-
-        /*weavedb-client*/
-        rpc: "http://localhost:8080",
-        // rpc: "http://lb.weavedb-node.xyz:80",
-        // rpc: "https://lb.weavedb-node.xyz:443",
+        rpc: "https://grpc.weavedb-node.xyz",
       });
-
-      const list = await sdk.get("nft", ["tokenID", "desc"], true);
-      console.log(list);
 
       if (clientGetOwner) {
         setOwner(await sdk.getOwner());
@@ -49,7 +39,7 @@ export default function Home() {
       }
 
       if (clientGetVersion) {
-        await sdk.getVersion();
+        console.log("getVersion()", await sdk.getVersion());
       }
 
       if (getDataNft) {
